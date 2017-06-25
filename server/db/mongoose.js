e9598e8f229bd3e12a4c1ee5eddff2cf6e46ccbe
AjithKrishnaAithal <ajith.aithal20890@gmail.com>
2017-06-25 20:58:50 +0530

    Final 2 method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://ajithk:Ajith2089020890@ds139082.mlab.com:39082/ak20890' );
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://ajithk:Ajith2089020890@ds139082.mlab.com:39082/ak20890' );
 
 module.exports={
   mongoose
